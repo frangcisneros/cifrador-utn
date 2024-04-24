0cfc7b0f3900c0823ec9b4900e7d5010cb053b75
Francisco Cisneros <franbichir@gmail.com>
2024-04-24 11:01:52 -0300

    cambios del profesor

--- app/models/user.py
+++ app/models/user.py
@@ -1,27 +1,63 @@
 # Importa el decorador dataclass desde el módulo dataclasses
 from dataclasses import dataclass
+
 # Importa la clase UserData desde el archivo user_data.py en el mismo directorio
 from .user_data import UserData
+
 # Importa la instancia db desde el módulo app, que parece ser un objeto de SQLAlchemy
 from app import db
+from typing import List
+
 
 # Define una clase llamada User utilizando el decorador dataclass
 @dataclass(init=False, repr=True, eq=True)
-class User(db.Model):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
-    __tablename__ = 'users'  # Nombre de la tabla en la base de datos
-    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Columna de clave primaria
-    username: str = db.Column(db.String(80), unique=True, nullable=False)  # Columna para el nombre de usuario
-    password: str = db.Column(db.String(120), nullable=False)  # Columna para la contraseña del usuario
-    email: str = db.Column(db.String(120), unique=True, nullable=False)  # Columna para el correo electrónico del usuario
-    
+class User(
+    db.Model
+):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
+    __tablename__ = "users"  # Nombre de la tabla en la base de datos
+    id: int = db.Column(
+        db.Integer, primary_key=True, autoincrement=True
+    )  # Columna de clave primaria
+    username: str = db.Column(
+        db.String(80), unique=True, nullable=False
+    )  # Columna para el nombre de usuario
+    password: str = db.Column(
+        db.String(120), nullable=False
+    )  # Columna para la contraseña del usuario
+    email: str = db.Column(
+        db.String(120), unique=True, nullable=False
+    )  # Columna para el correo electrónico del usuario
+
     # Relación con la tabla 'UserData' (datos de usuario), establecida a través de la propiedad 'user' en la clase UserData
-    data = db.relationship('UserData', uselist=False, back_populates='user') # type: ignore
+    data = db.relationship("UserData", uselist=False, back_populates="user")  # type: ignore
 
     # Constructor de la clase User, que puede recibir un objeto UserData opcionalmente
     def __init__(self, user_data: UserData = None):
-        # Si se proporciona un objeto UserData, se asigna a la propiedad 'data' del usuario
-        if user_data:
-            self.data = user_data
-        else:
-            # Si no se proporciona ningún objeto UserData, se crea uno nuevo y se asigna al usuario
-            self.data = UserData()
+        self.data = user_data
+
+    """
+    Aplico el patrón Active Record https://www.martinfowler.com/eaaCatalog/activeRecord.html, donde el modelo se encarga de la persistencia de los datos.
+    Este patrón es muy útil para aplicaciones pequeñas y medianas, pero no es recomendable para aplicaciones grandes.
+    Puede llegar a contradecir los principios SOLID http://butunclebob.com/ArticleS.UncleBob.PrinciplesOfOod, ya que el modelo tiene responsabilidades de persistencia y de negocio.
+    """
+
+    def save(self) -> "User":
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def all(cls) -> List["User"]:
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, id: int) -> "User":
+        return cls.query.get(id)
+
+    @classmethod
+    def find_by(cls, **kwargs) -> List["User"]:
+        return cls.query.filter_by(**kwargs).all()
--- test/test_user.py
+++ test/test_user.py
@@ -3,7 +3,14 @@ from flask import current_app
 from app import create_app, db
 from app.models import User, UserData
 
-class AppTestCase(unittest.TestCase):
+
+class UserTestCase(unittest.TestCase):
+    """
+    Test User model
+    Necesitamos aplicar principios como DRY (Don't Repeat Yourself) y KISS (Keep It Simple, Stupid).
+    YAGNI (You Aren't Gonna Need It) y SOLID (Single Responsibility Principle).
+    """
+
     def setUp(self):
         # Crea una instancia de la aplicación Flask para pruebas
         self.app = create_app()
@@ -23,32 +30,122 @@ class AppTestCase(unittest.TestCase):
     # Prueba si la aplicación Flask se crea correctamente
     def test_app(self):
         self.assertIsNotNone(current_app)
-    
+
     # Prueba la creación de un usuario
     def test_user(self):
         # Crea un objeto UserData con información de prueba
         data = UserData()
-        data.surname = 'surname'
-        data.address = 'address 1234'
-        data.city = 'city'
-        data.country = 'country'
-        data.phone = '542605502105'
-        
+        data.firstname = "Pablo"
+        data.lastname = "Prats"
+        data.address = "Address 1234"
+        data.city = "San Rafael"
+        data.country = "Argentina"
+        data.phone = "54260123456789"
+
         # Crea un objeto User y establece sus atributos
         user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'test'
-        user.password = 'test1234'
+        user.email = "test@test.com"
+        user.username = "pabloprats"
+        user.password = "Qvv3r7y"
 
         # Verifica que los atributos del usuario se hayan establecido correctamente
-        self.assertTrue(user.email, 'test@test.com')
-        self.assertTrue(user.username, 'test')
-        self.assertTrue(user.password, 'test1234')
+        self.assertTrue(user.email, "test@test.com")
+        self.assertTrue(user.username, "pabloprats")
+        self.assertTrue(user.password, "Qvv3r7y")
         self.assertIsNotNone(user.data)
-        self.assertTrue(user.data.surname, 'surname')
-        self.assertTrue(user.data.address, 'address 1234')
-        self.assertTrue(user.data.phone, '542605502105')   
+        self.assertTrue(user.data.address, "Address 1234")
+        self.assertTrue(user.data.firstname, "Pablo")
+        self.assertTrue(user.data.lastname, "Prats")
+        self.assertTrue(user.data.phone, "54260123456789")
 
-if __name__ == '__main__':
+    def test_user_save(self):
+
+        data = UserData()
+        data.firstname = "Pablo"
+        data.lastname = "Prats"
+        data.address = "Address 1234"
+        data.city = "San Rafael"
+        data.country = "Argentina"
+        data.phone = "54260123456789"
+
+        user = User(data)
+        user.email = "test@test.com"
+        user.username = "pabloprats"
+        user.password = "Qvv3r7y"
+
+        user.save()
+        self.assertGreaterEqual(user.id, 1)
+        self.assertTrue(user.email, "test@test.com")
+        self.assertTrue(user.username, "pabloprats")
+        self.assertTrue(user.password, "Qvv3r7y")
+        self.assertIsNotNone(user.data)
+        self.assertTrue(user.data.address, "Address 1234")
+        self.assertTrue(user.data.firstname, "Pablo")
+        self.assertTrue(user.data.lastname, "Prats")
+        self.assertTrue(user.data.phone, "54260123456789")
+
+    def test_user_delete(self):
+
+        data = UserData()
+        data.firstname = "Pablo"
+        data.lastname = "Prats"
+        data.address = "Address 1234"
+        data.city = "San Rafael"
+        data.country = "Argentina"
+        data.phone = "54260123456789"
+
+        user = User(data)
+        user.email = "test@test.com"
+        user.username = "pabloprats"
+        user.password = "Qvv3r7y"
+
+        user.save()
+
+        # borro el usuario
+        user.delete()
+        self.assertIsNone(User.find(user.id))
+
+    def test_user_all(self):
+
+        data = UserData()
+        data.firstname = "Pablo"
+        data.lastname = "Prats"
+        data.address = "Address 1234"
+        data.city = "San Rafael"
+        data.country = "Argentina"
+        data.phone = "54260123456789"
+
+        user = User(data)
+        user.email = "test@test.com"
+        user.username = "pabloprats"
+        user.password = "Qvv3r7y"
+        user.save()
+
+        users = User.all()
+        self.assertGreaterEqual(len(users), 1)
+
+    def test_user_find(self):
+
+        data = UserData()
+        data.firstname = "Pablo"
+        data.lastname = "Prats"
+        data.address = "Address 1234"
+        data.city = "San Rafael"
+        data.country = "Argentina"
+        data.phone = "54260123456789"
+
+        user = User(data)
+        user.email = "test@test.com"
+        user.username = "pabloprats"
+        user.password = "Qvv3r7y"
+        user.save()
+
+        user_find = User.find(1)
+        self.assertIsNotNone(user_find)
+        self.assertEqual(user_find.id, user.id)
+        self.assertEqual(user_find.email, user.email)
+
+
+if __name__ == "__main__":
     # Ejecuta el conjunto de pruebas si el script se ejecuta directamente
     unittest.main()
