0e326c48ae17cfc28a71ab6d374370a827686479
Francisco Cisneros <franbichir@gmail.com>
2024-04-24 22:24:03 -0300

    docker working

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,26 @@
 # Importaciones necesarias
 from flask import Flask  # Importa la clase Flask para crear la aplicación
-from flask_marshmallow import Marshmallow  # Importa la extensión Marshmallow para la serialización y deserialización de objetos
+from flask_marshmallow import (
+    Marshmallow,
+)  # Importa la extensión Marshmallow para la serialización y deserialización de objetos
 import os  # Importa el módulo os para interactuar con el sistema operativo
-from flask_migrate import Migrate  # Importa la extensión Flask-Migrate para migraciones de base de datos
-from flask_sqlalchemy import SQLAlchemy  # Importa la extensión SQLAlchemy para interactuar con bases de datos relacionales
-from app.config import config  # Importa la configuración de la aplicación desde el paquete app.config
+from flask_migrate import (
+    Migrate,
+)  # Importa la extensión Flask-Migrate para migraciones de base de datos
+from flask_sqlalchemy import (
+    SQLAlchemy,
+)  # Importa la extensión SQLAlchemy para interactuar con bases de datos relacionales
+from app.config import (
+    config,
+)  # Importa la configuración de la aplicación desde el paquete app.config
 
 # Crea instancias de las extensiones
 db = SQLAlchemy()  # Instancia de SQLAlchemy para interactuar con la base de datos
 migrate = Migrate()  # Instancia de Flask-Migrate para migraciones de base de datos
-ma = Marshmallow()  # Instancia de Marshmallow para la serialización y deserialización de objetos
+ma = (
+    Marshmallow()
+)  # Instancia de Marshmallow para la serialización y deserialización de objetos
+
 
 # Define una función para crear la aplicación Flask
 def create_app() -> Flask:
@@ -18,27 +29,38 @@ def create_app() -> Flask:
     Ref: Book Flask Web Development Page 78
     """
     # Obtiene el contexto de la aplicación desde la variable de entorno FLASK_CONTEXT
-    app_context = os.getenv('FLASK_CONTEXT')
-    
+    app_context = os.getenv("FLASK_CONTEXT")
+
     # Crea una instancia de la aplicación Flask
     app = Flask(__name__)  # El nombre del módulo actual se pasa como argumento
 
     # Obtiene la configuración correspondiente al contexto de la aplicación (desarrollo, producción, etc.)
-    config_instance = config.factory(app_context if app_context else 'development')
-    app.config.from_object(config_instance)  # Configura la aplicación con la instancia de configuración obtenida
+    config_instance = config.factory(app_context if app_context else "development")
+    app.config.from_object(
+        config_instance
+    )  # Configura la aplicación con la instancia de configuración obtenida
 
     # Inicializa las extensiones con la aplicación
     ma.init_app(app)  # Inicializa Marshmallow con la aplicación
     db.init_app(app)  # Inicializa SQLAlchemy con la aplicación
-    migrate.init_app(app, db)  # Inicializa Flask-Migrate con la aplicación y la instancia de SQLAlchemy
+    migrate.init_app(
+        app, db
+    )  # Inicializa Flask-Migrate con la aplicación y la instancia de SQLAlchemy
 
     # Registra los blueprints (enrutadores modulares) en la aplicación
-    from app.resources import home  # Importa el blueprint 'home' desde el paquete app.resources
-    app.register_blueprint(home, url_prefix='/api/v1')  # Registra el blueprint 'home' con un prefijo de URL '/api/v1'
+    from app.resources import (
+        home,
+    )  # Importa el blueprint 'home' desde el paquete app.resources
+
+    app.register_blueprint(
+        home, url_prefix="/api/v1"
+    )  # Registra el blueprint 'home' con un prefijo de URL '/api/v1'
 
     # Define un contexto de shell para interactuar con la aplicación en el shell interactivo
-    @app.shell_context_processor    
+    @app.shell_context_processor
     def shell_context():
-        return {"app": app}  # Devuelve un diccionario con la aplicación para el contexto de shell
+        return {
+            "app": app
+        }  # Devuelve un diccionario con la aplicación para el contexto de shell
 
     return app  # Retorna la aplicación Flask creada
--- app/config/config.py
+++ app/config/config.py
@@ -1,52 +1,70 @@
 # Importaciones necesarias
-from asyncio.log import logger  # Importa el registro asincrónico (no se utiliza en este código)
-from dotenv import load_dotenv  # Importa la función para cargar variables de entorno desde un archivo .env
-from pathlib import Path  # Importa la clase Path de la biblioteca pathlib para trabajar con rutas de archivos
+from asyncio.log import (
+    logger,
+)  # Importa el registro asincrónico (no se utiliza en este código)
+from dotenv import (
+    load_dotenv,
+)  # Importa la función para cargar variables de entorno desde un archivo .env
+from pathlib import (
+    Path,
+)  # Importa la clase Path de la biblioteca pathlib para trabajar con rutas de archivos
 import os  # Importa el módulo os para interactuar con el sistema operativo
 
 # Obtiene la ruta absoluta del directorio base de la aplicación
 basedir = os.path.abspath(Path(__file__).parents[2])
 
 # Carga las variables de entorno desde un archivo .env en el directorio base de la aplicación
-load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv(os.path.join(basedir, ".env"))
+
 
 # Clase base para la configuración de la aplicación
 class Config(object):
     TESTING = False  # Modo de pruebas desactivado por defecto
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactiva el seguimiento de modificaciones en SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = (
+        False  # Desactiva el seguimiento de modificaciones en SQLAlchemy
+    )
     SQLALCHEMY_RECORD_QUERIES = True  # Activa el registro de consultas en SQLAlchemy
-    
+
     # Método estático para inicializar la aplicación con esta configuración
     @staticmethod
     def init_app(app):
         pass  # No hace nada por defecto
 
+
 # Clase de configuración para el entorno de desarrollo
 class DevelopmentConfig(Config):
     TESTING = True  # Modo de pruebas activado
     DEBUG = True  # Modo de depuración activado
-    SQLALCHEMY_TRACK_MODIFICATIONS = True  # Activa el seguimiento de modificaciones en SQLAlchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = (
+        True  # Activa el seguimiento de modificaciones en SQLAlchemy
+    )
     # Obtiene la URI de la base de datos de las variables de entorno
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
-        
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI")
+
+
 # Clase de configuración para el entorno de producción
 class ProductionConfig(Config):
     DEBUG = False  # Modo de depuración desactivado
     TESTING = False  # Modo de pruebas desactivado
-    SQLALCHEMY_RECORD_QUERIES = False  # Desactiva el registro de consultas en SQLAlchemy
+    SQLALCHEMY_RECORD_QUERIES = (
+        False  # Desactiva el registro de consultas en SQLAlchemy
+    )
     # Obtiene la URI de la base de datos de las variables de entorno
-    SQLALCHEMY_DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_DATABASE_URI")
+
     # Método de inicialización de la aplicación para el entorno de producción
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)  # Llama al método init_app de la clase base
 
+
 # Función de fábrica que devuelve la clase de configuración según el entorno especificado
 def factory(app):
     configuration = {
-        'development': DevelopmentConfig,  # Clase de configuración para el entorno de desarrollo
-        'production': ProductionConfig  # Clase de configuración para el entorno de producción
+        "development": DevelopmentConfig,  # Clase de configuración para el entorno de desarrollo
+        "production": ProductionConfig,  # Clase de configuración para el entorno de producción
     }
-    
-    return configuration[app]  # Devuelve la clase de configuración correspondiente al entorno
+
+    return configuration[
+        app
+    ]  # Devuelve la clase de configuración correspondiente al entorno
